a254b3d330b04c47c167a8150e8f2253a6de19ff
Sylvain Plessis <sylvain.plessis@gmail.com>
2014-07-23 15:17:25 -0500

    tests are with Antioch::KineticsConditions<,> object

--- test/threebody_process_unit.C
+++ test/threebody_process_unit.C
@@ -67,6 +67,9 @@ int tester()
 
   for(Scalar T = 300.1; T <= 2500.1; T += 10.)
   {
+
+    const Antioch::KineticsConditions<Scalar> conditions(T);
+
     for(unsigned int ikinmod = 0; ikinmod < 6; ikinmod++)
     {
 
@@ -159,12 +162,12 @@ int tester()
     {
         TB_reaction->set_efficiency("",i,species_eff[i]);
     }
-    Scalar rate1 = TB_reaction->compute_forward_rate_coefficient(mol_densities,T);
+    Scalar rate1 = TB_reaction->compute_forward_rate_coefficient(mol_densities,conditions);
     Scalar rate;
     Scalar drate_dT;
     std::vector<Scalar> drate_dx;
     drate_dx.resize(n_species);
-    TB_reaction->compute_forward_rate_coefficient_and_derivatives(mol_densities,T,rate,drate_dT,drate_dx);
+    TB_reaction->compute_forward_rate_coefficient_and_derivatives(mol_densities,conditions,rate,drate_dT,drate_dx);
 
 
     for(unsigned int i = 0; i < n_species; i++)
